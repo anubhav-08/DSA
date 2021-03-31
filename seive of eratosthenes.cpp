fe13ad6dd803b5fdf49280457ebce8e1ad69ed9e
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-03-31 11:37:59 +0530

    seive

---
+++ seive of eratosthenes.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp> 
+//#include <ext/pb_ds/standard_policy.hpp>
+//using namespace pb_ds; 
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_back
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     que_max         priority_queue <int>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+std::vector<bool> isPrime(1e6+1, true);
+void storePrime()
+{
+    int n = 1e6;
+    isPrime[0] = isPrime[1] = false;
+    for(int i = 2; i*i<=n; i++)
+    {
+        if(isPrime[i])
+        {
+            for(int j = i*i; j <= n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    storePrime();
+    fo(i,0,100)
+    {
+        cout<<i<<" : "<<isPrime[i]<<endl;
+    }
+    return 0;
+}
