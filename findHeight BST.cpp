f7c43fb1bc65ee8c0c274476b838dfa51c370201
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-07-17 20:48:31 +0530

    bst height

---
+++ findHeight BST.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp> 
+//#include <ext/pb_ds/standard_policy.hpp>
+//using namespace pb_ds; 
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_bac
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     que_max         priority_queue <int>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = NULL;
+    }
+};
+
+
+Node* insertNode(Node *root, int data)
+{
+    if(root == NULL)
+    {
+        root = new Node(data);
+    }
+    else if(root->data > data)
+    {
+        root->left = insertNode(root->left, data);
+    }
+    else
+    {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
+}
+
+int bstHeight(Node *root)
+{
+    if(root == NULL)
+    {
+        return -1;
+    }
+    return max(bstHeight(root->left), bstHeight(root->right)) + 1;
+}
+//  using recursion
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+
+    Node *root = NULL;
+    root = insertNode(root, 5);
+    root = insertNode(root, 2);
+    root = insertNode(root, 6);
+    root = insertNode(root, 1);
+    root = insertNode(root, 4);
+
+    cout<<bstHeight(root);
+
+    return 0;
+} 
+
