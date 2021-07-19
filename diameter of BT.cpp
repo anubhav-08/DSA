89b781e3949b8883b9e05fd4126c9687a22a17dc
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-07-19 17:33:36 +0530

    dia of binary tree

---
+++ diameter of BT.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp> 
+//#include <ext/pb_ds/standard_policy.hpp>
+//using namespace pb_ds; 
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_back
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     que_max         priority_queue <int>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = NULL;
+    }
+};
+
+
+Node* insertNode(Node *root, int data)
+{
+    if(root == NULL)
+    {
+        root = new Node(data);
+    }
+    else if(root->data > data)
+    {
+        root->left = insertNode(root->left, data);
+    }
+    else
+    {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
+}
+
+pair<int, int> diameter(Node *root)
+{
+    if(root == NULL)
+    {
+        pair<int, int> temp = mp(0, -1);
+        return temp;
+    }
+
+    pair<int, int> tl, tr, ans;
+    tl = diameter(root->left);
+    tr = diameter(root->right);
+    ans.S = max(tl.S, tr.S) + 1;
+    ans.F = max(tl.S + tr.S + 2, max(tl.F, tr.F));
+    return ans;
+}
+
+//  using recursion
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+
+    Node *root = NULL;
+    root = insertNode(root, 5);
+    root = insertNode(root, 2);
+    root = insertNode(root, 6);
+    root = insertNode(root, 1);
+    root = insertNode(root, 4);
+
+    pair<int, int> t = diameter(root);
+    cout<<t.F<<endl;
+
+    return 0;
+} 
+
