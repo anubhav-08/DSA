c4d1bbf199ca2fcc7e76e55df93ee6be0108c31f
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-07-23 14:01:25 +0530

    k largest elements

---
+++ k largest elements.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp> 
+//#include <ext/pb_ds/standard_policy.hpp>
+//using namespace pb_ds; 
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_bac
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     max_heap        priority_queue <int>
+#define     min_heap        priority_queue <int, vector<int>, greater<int>>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+
+
+//  using recursion
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    int n, k;
+    cin>>n>>k;
+    std::vector<int> v(n);
+    fo(i,0,n)cin>>v[i];
+
+    min_heap minh;
+    fo(i,0,n)
+    {
+        minh.push(v[i]);
+        if(minh.size() > k)
+        {
+            minh.pop();
+        }
+    }
+    while(minh.size() > 0)
+    {
+        cout<<minh.top()<<endl;
+        minh.pop();
+    }
+    return 0;
+} 
+
