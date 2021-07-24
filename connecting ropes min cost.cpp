691b3803c449766a7e4f1d7933cd477efd365164
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-07-24 12:48:21 +0530

    connecting ropes minm cost

---
+++ connecting ropes min cost.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_bac
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     max_heap        priority_queue
+#define     min_heap(Type)  priority_queue <Type, vector<Type>, greater<Type>>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+typedef pair<int, int> pi;
+typedef pair<int, pair<int,int>> ppi;
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    int n;
+    cin>>n;
+    std::vector<int> v(n);
+    fo(i,0,n)cin>>v[i];
+
+    min_heap(int) minh;
+
+    fo(i,0,n)minh.push(v[i]);
+    int a, b, ans = 0;
+    while(minh.size() > 1)
+    {
+        a = minh.top();
+        minh.pop();
+        b = minh.top();
+        minh.pop();
+        minh.push(a+b);
+        ans += a+b;
+    }
+    cout<<ans<<endl;
+    return 0;
+} 
+
