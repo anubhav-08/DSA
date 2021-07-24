49a2cd8d6f42af2cd64e329c7c23091d1e8f8b1a
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-07-24 12:57:04 +0530

    closest point to origin

---
+++ closest points to origin.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_bac
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     max_heap        priority_queue
+#define     min_heap(Type)  priority_queue <Type, vector<Type>, greater<Type>>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+typedef pair<int, int> pi;
+typedef pair<int, pair<int,int>> ppi;
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    int n;
+    cin>>n;
+    vector<pi> v(n);
+    fo(i,0,n)
+    {
+        int a, b;
+        cin>>a>>b;
+        v[i] = mp(a, b);
+    }
+    max_heap<ppi> maxh;
+    fo(i,0,n)
+    {
+        int d = (v[i].F*v[i].F) + (v[i].S*v[i].S);
+        maxh.push(mp(d, v[i]));
+        if(maxh.size() > 2)
+            maxh.pop();
+    }
+
+    while(!maxh.empty())
+    {
+        cout<<maxh.top().S.F<<" "<<maxh.top().S.S<<endl;
+        maxh.pop();
+    }
+
+    return 0;
+} 
+
