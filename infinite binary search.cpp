96ba7fed7190980df17438c2e5028a666e1a4591
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-07-27 14:06:02 +0530

    infinite binary search

---
+++ infinite binary search.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_bac
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     max_heap        priority_queue
+#define     min_heap(Type)  priority_queue <Type, vector<Type>, greater<Type>>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+typedef pair<int, int> pi;
+typedef pair<int, pair<int,int>> ppi;
+
+int binarySearch(vector<int> v, int l, int r, int key)
+{
+    cout<<l<<" "<<r<<endl;
+    while(l<=r)
+    {
+        int mid  = (l+r)/2;
+        if(v[mid] == key)return mid;
+
+        if(v[mid] > key)r = mid-1;
+        else l = mid + 1;
+    }
+    return -1;
+}
+int infBinarySearch(vector<int> v, int key)
+{
+    int l = 0, r = 1;
+    while(r < v.size())
+    {
+        if(v[r] >= key)break;
+        l = r;
+        r *= 2;
+    }
+    return binarySearch(v, l, r, key);
+}   
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    fo(i,0,n)cin>>v[i];
+    cout<<infBinarySearch(v, 20);
+    return 0;
+} 
+
