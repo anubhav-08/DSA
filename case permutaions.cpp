a9732bccf5ae3874ee02ef8cd630782666b99652
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-08-05 19:10:50 +0530

    case permutation

---
+++ case permutaions.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_back
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     max_heap        priority_queue
+#define     min_heap(Type)  priority_queue <Type, vector<Type>, greater<Type>>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+typedef pair<int, int> pi;
+typedef pair<int, pair<int,int>> ppi;
+
+void casePermute(string s, string res)
+{
+    if(s.length() == 0)
+    {
+        cout<<res<<endl;
+        return;
+    }
+    char c = toupper(s[0]);
+    casePermute(s.substr(1, s.length()-1), res+s[0]);
+    casePermute(s.substr(1, s.length()-1), res + c);
+}
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    string s;
+    cin>>s;
+    casePermute(s, "");
+    return 0;
+} 
+
