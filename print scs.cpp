1775c80024a54e352c164d8903c4560961b11206
Anubhav Tyagi <tyagianubhav80@gmail.com>
2021-09-01 23:33:22 +0530

    print scs

---
+++ print scs.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define     mod            (int)1e9+7
+#define     MOD             998244353
+#define     ll              long long
+#define     mp              make_pair
+#define     pb              push_back
+#define     F               first
+#define     S               second
+#define     t()             int test;cin>>test;while(test--)
+#define     ii              pair<int,int>
+#define     max_heap        priority_queue
+#define     min_heap(Type)  priority_queue <Type, vector<Type>, greater<Type>>
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     endl            "\n"
+#define     fo(i,a,n)       for(int i=a; i<n; i++)
+
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+
+typedef pair<int, int> pi;
+typedef pair<int, pair<int,int>> ppi;
+
+string printScs(string x, string y)
+{
+    int n = x.length(), m = y.length();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    fo(i,1,n+1)
+    {
+        fo(j,1,m+1)
+        {
+            if(x[i-1] == y[j-1])dp[i][j] = 1+dp[i-1][j-1];
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    string ans = "";
+    while(m > 0 || n > 0)
+    {
+        if(x[n-1] == y[m-1])
+        {
+            ans += x[n-1];
+            n--;
+            m--;
+        }
+        else
+        {
+            if(dp[n-1][m] > dp[n][m-1])
+            {
+                ans += x[n-1];
+                n--;
+            }
+            else
+            {
+                ans += y[m-1];
+                m--;
+            }
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+ 
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    IOS;
+    string x, y;
+    cin>>x>>y;
+    cout<<printScs(x, y);
+    return 0;
+} 
+
